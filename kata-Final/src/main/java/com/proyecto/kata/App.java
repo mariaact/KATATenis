3c62ce720bc3e78a0b172f2a7f767594f2a9cd41
mariaact <mariactyermo@gmail.com>
2022-12-14 21:25:03 +0100

    subir el proyecto

---
+++ kata-Final/src/main/java/com/proyecto/kata/App.java
@@ -0,0 +1,13 @@
+package com.proyecto.kata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
